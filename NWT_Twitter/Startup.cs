21768d21fa281f9a034c32115fa74880387e4d2b
Zrinka Fiamengo <zfiame00@fesb.hr>
2015-10-20 22:44:55 +0200

    Initial Commit

---
+++ NWT_Twitter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NWT_Twitter.Startup))]
+namespace NWT_Twitter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
