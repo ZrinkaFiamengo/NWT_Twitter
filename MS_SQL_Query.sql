3f0035576c7cf912d855115bb4db81f5d6be3e2a
AliasNoName <bojcicm@gmail.com>
2015-11-26 16:56:59 +0100

    Update MS_SQL_Query.sql

--- MS_SQL_Query.sql
+++ MS_SQL_Query.sql
@@ -2,70 +2,54 @@
  SQL querys for creating DB in MS-SQL
 */
 
-create table Location(
-	ID			int not null unique,
-	Name		varchar(50) not null,
-	Geo_width	float not null,
-	Geo_height	float not null,
+create database Twitter
 
-	primary key (ID)
-); 
+create table Location(
+	ID			int				not null primary key identity,
+	Name		varchar(50)		not null,
+	Geo_width	float			not null,
+	Geo_height	float			not null
+)
 
 create table Users(
-	ID			int not null unique,
-	Name		varchar(20) not null,
-	Surname		varchar(20) not null,
-	Nickname	varchar(20) not null,
-	Picture_URL	varchar(max),
-	Location	int,
-
-	primary key (ID),
-	foreign key (Location) references Location(ID)
-); 
+	Id			int 			not null primary key identity,
+	Name		nvarchar(20)	not null,
+	Surname		nvarchar(20)	not null,
+	Nickname	nvarchar(20)	not null,
+	Picture_URL	nvarchar(max),
+		
+	Location	int			references Location(ID)
+)
 
 create table Tweet(
-	ID			int not null unique,
-	Text		varchar(140) not null,
-	Author		int not null
+	ID			int				not null primary key identity,
+	Text		varchar(140)	not null,
 
-	primary key (ID),
-	foreign key (Author) references Users(ID)
-); 
+	Author		int				not null references Users(Id)
+)
 
 create table Hashtag(
-	ID		int not null unique,
-	Text	varchar(139) not null,
-
-	primary key (ID)
-);
+	ID			int				not null primary key identity,
+	Text		nvarchar(139)	not null
+)
 
 create table Comment(
-	ID		int not null unique,
-	Text	varchar(max) not null,
-	Tweet	int,
-	Author	int,
+	ID			int				not null primary key identity,
+	Text		nvarchar(max)	not null,
 
-	primary key (ID),
-	foreign key (Tweet)		references Tweet(ID),
-	foreign key (Author)	references Users(ID)
-);
+	Tweet		int				references Tweet(ID),
+	Author		int				references Users(ID)
+)
 
 create table FavouriteTweet(
-	ID				int not null unique,
-	User_favourite	int not null,		/*ovo je osoba koja klika favourite*/
-	Tweet			int not null,
-
-	primary key (ID),
-	foreign key (User_favourite)	references Users(ID),
-	foreign key (Tweet)				references Tweet(ID),
-);
+	Id				int			not null primary key identity,
+	
+	User_favourite	int			not null references Users(Id),		/*ovo je osoba koja klika favourite*/
+	Tweet			int			not null references Tweet(ID)
+)
 
 create table TweetHashtagConnection (
-	ID		int not null unique,
-	Tweet	int not null,
-	Hashtag	int not null,
-
-	primary key (ID),
-	foreign key (Tweet)		references Tweet(ID),
-	foreign key (Hashtag)	references Hashtag(ID),
-);
+	ID			int				not null primary key identity,
+	Tweet	int					not null references Tweet(ID),
+	Hashtag	int					not null references Hashtag(ID)
+)
